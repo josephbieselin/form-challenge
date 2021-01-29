249e5e7861fc18e49664ca299425e565d49c7c87
Joe Bieselin <joebieselin@gmail.com>
2021-01-29 09:07:32 -0800

    docs(formHandler): comment for password
    * no backend component to challenge, but noting where password management would happen

--- scripts/formHandler.js
+++ scripts/formHandler.js
@@ -4,6 +4,8 @@ function onSubmit() {
     const firstName = inputs["firstName"].value;
     const email = inputs["email"].value;
 
+    /* handle password here if there were backend component to challenge */
+    
     // route to confirmation page
     const url = location.href + "confirmation.html?firstName=" + firstName + "&email=" + email;
     location.assign(url);
